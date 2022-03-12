5bdf9d36b05631969a76a58393b4395eea8490ab
vuvanan12 <101451719+vuvanan12@users.noreply.github.com>
2022-03-12 15:50:42 +0700

    Add files via upload

---
+++ baitap.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "Ho va ten: VU VAN AN " << '\n'
+         << "MSSV: 106210113" << '\n'
+         << "Lop: 21DTCLC2";
+    return 0;
+}
